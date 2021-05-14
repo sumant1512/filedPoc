cbafd248c8405780fd4bd75de031a6637057d3bd
sumant mishra <sumantmishra511@gmail.com>
2021-05-15 02:23:16 +0530

    payment gateway added

--- src/app/users/user-card/user-card.component.ts
+++ src/app/users/user-card/user-card.component.ts
@@ -8,7 +8,47 @@ import { UserDetailsType } from "src/app/store/user/types/user.type";
 })
 export class UserCardComponent implements OnInit {
   @Input() userDetail: UserDetailsType;
+  handler: any = null;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadStripePaymentGateway();
+  }
+
+  payAmountForAds(amount: any): void {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: "pk_test_51Ir7LOSBEzlXwWbZKaYP21A6WQji2pmQpLBAU1fxRiTN2LMMD89xihR4AojiEUHi4coeqpJhF3yzWqkdtpOJCOeK00hvze8aya",
+      locale: "auto",
+      token: function (token: any) {
+        alert("Token Created!!");
+      },
+    });
+
+    handler.open({
+      name: "Mock payment Gateway",
+      description: "Client Side Only",
+      amount: amount * 100,
+    });
+  }
+
+  loadStripePaymentGateway(): void {
+    if (!window.document.getElementById("stripe-script")) {
+      var server = window.document.createElement("script");
+      server.id = "stripe-script";
+      server.type = "text/javascript";
+      server.src = "https://checkout.stripe.com/checkout.js";
+      server.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: "pk_test_51Ir7LOSBEzlXwWbZKaYP21A6WQji2pmQpLBAU1fxRiTN2LMMD89xihR4AojiEUHi4coeqpJhF3yzWqkdtpOJCOeK00hvze8aya",
+          locale: "auto",
+          token: function (token: any) {
+            alert("Payment Success!!");
+          },
+        });
+      };
+
+      window.document.body.appendChild(server);
+    }
+  }
 }
